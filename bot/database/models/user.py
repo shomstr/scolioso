29d31bb32f39597f0876a769eae4af3149c8aa26
NotBupyc <notbupyc@gmail.com>
2024-09-26 21:59:29 +0300

    init

--- bot/database/models/user.py
+++ bot/database/models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import BigInteger, Column, String
+from datetime import datetime
+
+from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.orm import mapped_column, Mapped
 from .base import BaseModel
 
@@ -6,5 +8,10 @@ from .base import BaseModel
 class User(BaseModel):
     __tablename__ = "users"
 
-    user_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
     username: Mapped[str] = Column(String(32), nullable=True)
+
+    petals: Mapped[int] = mapped_column(Integer, server_default="0")
+    water: Mapped[int] = mapped_column(Integer, server_default="0")
+    len_tree: Mapped[int] = mapped_column(Integer, server_default="0")
+
+    last_walk: Mapped[datetime] = mapped_column(DateTime())
