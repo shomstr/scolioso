6d5bf9930cc1d1300b98257eb9e9664a6dd4f851
NotBupyc <notbupyc@gmail.com>
2024-09-26 23:41:01 +0300

    add: walk

--- bot/handlers/users/main_menu.py
+++ bot/handlers/users/main_menu.py
@@ -1,11 +1,15 @@
 import logging
+import random
+from datetime import datetime
+from typing import Any
 
 from aiogram import Router, F
 from aiogram.types import Message
+from asyncpg.pgproto.pgproto import timedelta
 
 from bot.database.models import User
 from bot.enums.menus import MainMenu
-from bot.messages import BAG_TEXT
+from bot.messages import BAG_TEXT, WALK_TEXTS
 
 router = Router()
 logger = logging.getLogger()
@@ -15,3 +19,21 @@ logger = logging.getLogger()
 async def bag(message: Message, user: User):
     text = BAG_TEXT.format(user=user)
     await message.reply(text)
+
+
+@router.message(F.text == MainMenu.WALK)
+async def walk(message: Message, user: User) -> Any:
+    if user.last_walk and user.last_walk + timedelta(hours=12) > datetime.now():
+        return await message.answer("Еще рано для прогулки")
+
+    user.last_walk = datetime.now()
+    petals = random.randint(1, 10)
+    water = random.randint(1, 5)
+    user.petals += petals
+    user.water += water
+
+    t = random.choice(WALK_TEXTS)
+    text = t.format(petals=petals, water=water)
+
+    await message.reply(text)
+    return None
