ef6bab0700eff8fa2bccf18ffd72943d76fd0c1f
Bupyc <notbupyc@gmail.com>
2024-10-15 18:33:05 +0300

    add: конвертер

--- bot/handlers/users/__init__.py
+++ bot/handlers/users/__init__.py
@@ -4,8 +4,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from aiogram import Router
 
-from . import start_help, main_menu, top
+from . import start_help, main_menu, top, converter
 
-routers: list[Router] = [start_help.router, main_menu.router, top.router]
+routers: list[Router] = [start_help.router, main_menu.router, top.router, converter.router]
 
 _all__ = ["routers"]
---
+++ bot/handlers/users/converter.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Any
+
+from aiogram import Router
+from aiogram.types import Message
+
+from bot.config import WATER_TO_APPLE, APPLE_TO_WATER
+from bot.database.models import User
+from bot.filters import FullmatchWithArgs
+
+router = Router()
+logger = logging.getLogger()
+
+
+@router.message(FullmatchWithArgs("купить яблоко", "купить яблоки", "купить яблок", user=False))
+async def buy_apple(message: Message, user: User, count: int) -> Any:
+    if user.water < WATER_TO_APPLE * count:
+        return await message.reply("Недостаточно воды для покупки")
+
+    user.water -= WATER_TO_APPLE * count
+    user.apples += count
+
+    await message.reply(f"Вы купили {count} 🍏  за {WATER_TO_APPLE * count} 💧")
+    return None
+
+
+@router.message(FullmatchWithArgs("купить воду", "купить воды", user=False))
+async def buy_water(message: Message, user: User, count: int) -> Any:
+    if user.apples < APPLE_TO_WATER * count:
+        return await message.reply("Недостаточно яблок для покупки")
+
+    user.apples -= count
+    user.water += WATER_TO_APPLE * count
+
+    await message.reply(f"Вы купили {WATER_TO_APPLE * count} 💧 за {count} 🍏 ")
+    return None
