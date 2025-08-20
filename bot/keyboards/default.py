8060728d7f5e15a5ec851d2c45ca6aa09a606063
puffa <schamil.gab@yandex.ru>
2025-08-20 13:18:25 +0300

    Update frontend submodule to latest commit

--- bot/keyboards/default.py
+++ bot/keyboards/default.py
@@ -1,23 +1,12 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import Message, ReplyKeyboardRemove, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+#from core.data.tricks.start_tricks import start_tricks as tricks
 
-from bot.enums.menus import MainMenu
-
-
-main_menu = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text=MainMenu.WALK), KeyboardButton(text=MainMenu.WATERING)],
-        [KeyboardButton(text=MainMenu.BAG)],
-    ],
-    resize_keyboard=True,
-    selective=True,
-)
-
-
-main2_menu = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='Мужской'), KeyboardButton(text='Женский')],
-        
-    ],
-    resize_keyboard=True,
-    selective=True,
-)
+def main2_menu():
+    kb = ReplyKeyboardBuilder()
+    
+    kb.button(text='Мужской'),
+    kb.button(text='Женский'),
+    
+    kb.adjust(1),
+    return kb.as_markup(resize_keyboard=True)
